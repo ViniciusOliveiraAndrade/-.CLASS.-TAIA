29048c99df97e8ec21e4d15453deddaa6536dc93
Vinícius de Oliveira Andrade <viniciusibsm@gmail.com>
2020-01-03 17:11:52 -0300

    Update CapturarComentarioFacebook.py

--- André/CapturarComentarioFacebook.py
+++ André/CapturarComentarioFacebook.py
@@ -8,7 +8,7 @@ def capcom(id_objeto,qt_comentario = 5000):
 	objeto = str(id_objeto) #id do post
 	campos = "comments.limit("+str(qt_comentario)+")" #campo que quer e quantidade
 	versao = "v2.9/" #versão da api
-	access_token = "EAABZC6IpUAPgBAOfIIyOD38qzKLWRE1ggGnwErbaIZB62e9Pe9BKW8wmGhTCc8o0gvJEvs0TBIfLRLk0wTCu6Y1j1eMSUIZABcy9byhfgLFxoDZAKiFZCgcphlBBrsF9UIq6FNKsA0UPzNA08ZAyNuJNsWBf3uuIMZD" #token de aceo
+	access_token = "" #token de aceo
 
 	url = "%s%s%s?fields=%s&access_token=%s" % (base_url, versao, objeto, campos, access_token) #junção e formação da url completa
 
